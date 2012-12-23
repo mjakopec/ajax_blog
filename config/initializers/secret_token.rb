562f0d1f29362710b739b2223b27c4e8c53a8a64
Mario Jakopec <mario@faveo.hr>
2012-12-23 21:57:09 +0100

    Update config/initializers/secret_token.rb

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-AjaxBlog::Application.config.secret_token = 'bc9b382ff619aac08bcbb609f85f0769243ac4d6dea49694442d6670043a4723656aa806b075086e18166e501b72ed52c075eb290dbb07d43af7561b30126a6d'
